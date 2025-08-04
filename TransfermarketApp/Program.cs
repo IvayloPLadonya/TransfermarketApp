319f7c340191abcb4feac8f01958e4d425fa6327
IvayloPLadonya <ivailoladonia@gmail.com>
2025-08-04 21:35:43 +0300

    errorHandling

--- TransfermarketApp/Program.cs
+++ TransfermarketApp/Program.cs
@@ -38,9 +38,9 @@ namespace TransfermarketApp
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+				app.UseExceptionHandler("/Error/500");
+				app.UseStatusCodePagesWithReExecute("/Error/{0}");
+				app.UseHsts();
             }
 
             app.UseHttpsRedirection();
